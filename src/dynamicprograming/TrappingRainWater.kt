d4b5f2246b1f8c666ddb7c6819564ef08286ae21
TungVD <tungtd95@gmail.com>
2021-10-26 20:39:51 +0700

    Trapping rain water

---
+++ src/dynamicprograming/TrappingRainWater.kt
@@ -0,0 +1,45 @@
+package dynamicprograming
+
+class TrappingRainWater {
+    fun trap(height: IntArray): Int {
+        val maxIdx = findMaxIdxInRange(height, 1, height.size)
+        return trapInRange(height, 1, maxIdx, false) +
+                trapInRange(height, maxIdx, height.size, true)
+    }
+
+    private fun trapInRange(height: IntArray, start: Int, end: Int, isMaxStart: Boolean): Int {
+        if (start == end || start == end - 1) return 0
+        val sum: Int =
+            if (isMaxStart) {
+                val maxIdx = findMaxIdxInRange(height, start + 1, end)
+                sumInRange(height, start, maxIdx, maxIdx) + trapInRange(height, maxIdx, end, true)
+            } else {
+                val maxIdx = findMaxIdxInRange(height, start, end - 1)
+                sumInRange(height, maxIdx, end, maxIdx) + trapInRange(height, start, maxIdx, false)
+            }
+
+        return sum
+    }
+
+    private fun sumInRange(height: IntArray, start: Int, end: Int, low: Int): Int {
+        var sum = (end - start - 1) * height[low - 1]
+        for (i in start + 1 until end) {
+            sum -= height[i - 1]
+        }
+        return sum
+    }
+
+    private fun findMaxIdxInRange(height: IntArray, start: Int, end: Int): Int {
+        var max = height[start - 1]
+        var maxIdx = start
+
+        for (i in start..end) {
+            if (height[i - 1] > max) {
+                max = height[i - 1]
+                maxIdx = i
+            }
+        }
+
+        return maxIdx
+    }
+}
--- src/test/dynamic_programing_test.kt
+++ src/test/dynamic_programing_test.kt
@@ -60,6 +60,13 @@ class DynamicProgramingTest {
     }
 
     @Test
+    fun `test trapping rain water serious`() {
+        assertEquals(10, TrappingRainWater().trap(intArrayOf(3, 0, 0, 2, 0, 4)))
+        assertEquals(10, TrappingRainWater().trap(intArrayOf(7, 4, 0, 9)))
+        assertEquals(0, TrappingRainWater().trap(intArrayOf(6, 9, 9)))
+    }
+
+    @Test
     fun `test maximize the cut segment`() {
         assertEquals(4, MaximizeTheCutSegment().maximizeCuts(4, 2, 1, 1))
         assertEquals(2, MaximizeTheCutSegment().maximizeCuts(5, 5, 3, 2))
